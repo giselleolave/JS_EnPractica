70fa80919dd27420c87a5243f44251ddc3d548e0
Giselle Olave <gisellevalentinaolave@gmail.com>
2024-03-07 13:32:23 -0300

    Ejercicio 3 corregido

--- assets/js/app3.js
+++ assets/js/app3.js
@@ -1,30 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
- 
- let sumar = document.getElementById('btn-sumar');
- let restar = document.getElementById('btn-restar');
-
+    let sumar = document.getElementById('btn-sumar');
+    let restar = document.getElementById('btn-restar');
 
     sumar.addEventListener("click", function() {
-        let valor1= document.querySelector('#valor1').value;
-        let valor2= document.querySelector('#valor2').value;
-        valor1 = isNaN(valor1) ? 0 : parseInt(valor1);                  
-        valor2 =isNaN(valor2) ? 0 : parseInt(valor2);
-        document.querySelector(".resultado").innerHTML= (parseInt(valor1) + parseInt(valor2));
+        let valor1 = document.querySelector('#valor1').value;
+        let valor2 = document.querySelector('#valor2').value;
         
+        valor1 = /^\d+$/.test(valor1) ? parseInt(valor1) : 0;
+        valor2 = /^\d+$/.test(valor2) ? parseInt(valor2) : 0;
+        
+        document.querySelector(".resultado").innerHTML = valor1 + valor2;
     });
 
     restar.addEventListener("click", function() {
-        let valor1= document.querySelector('#valor1').value;
-        let valor2= document.querySelector('#valor2').value;
-        valor1 = isNaN(valor1) ? 0 : parseInt(valor1);                  
-        valor2 =isNaN(valor2) ? 0 : parseInt(valor2);
+        let valor1 = document.querySelector('#valor1').value;
+        let valor2 = document.querySelector('#valor2').value;
+        
+        valor1 = /^\d+$/.test(valor1) ? parseInt(valor1) : 0;
+        valor2 = /^\d+$/.test(valor2) ? parseInt(valor2) : 0;
+        
         let resultado = valor1 - valor2;
         if (resultado < 0) {
             resultado = 0;
         }
-        document.querySelector(".resultado").innerHTML= resultado;
-
+        
+        document.querySelector(".resultado").innerHTML = resultado;
     });
-
-
 });
